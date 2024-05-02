4a9bee3d8ba55b9c53c86854b19be2a6cff94677
Michael Lombardi <mikelombardi4332@gmail.com>
2024-05-02 00:32:58 -0500

    fixed act3

--- sa18-act3/StoreApp/StoreApp/urls.py
+++ sa18-act3/StoreApp/StoreApp/urls.py
@@ -16,15 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-from django.urls import path
-from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('products/', views.index, name='index'),
     path('products/<int:id>/', views.show, name='show'),
 ]
